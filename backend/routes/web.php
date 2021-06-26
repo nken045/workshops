4ebef4c67fdfe60c47a4ec03d47d38538757db2f
liorange0119 <orange.riisa0119@gamil.com>
2021-06-26 22:16:23 +0900

    管理画面の実装

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+//管理画面
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
